1d52c462325be2f47adc3ae0dfcc2dc15df38495
scanQRC <scaninfoQR@gmail.com>
2026-07-04 20:08:45 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,16 @@
+-- ==========================================================
+-- SCANME QR
+-- Production Database Schema
+-- MySQL 8.x
+-- ==========================================================
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+CREATE DATABASE IF NOT EXISTS scanmeqr
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+USE scanmeqr;
+
+-- Further tables will be added here.
